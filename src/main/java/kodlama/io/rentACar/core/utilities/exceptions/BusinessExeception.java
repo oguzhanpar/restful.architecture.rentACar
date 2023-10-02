0704ebdec75460f3b610326ea2784f743df710f8
oguzhanpar <oguzhan.parlak@yahoo.com>
2023-10-02 15:47:16 +0300

    first commit

---
+++ src/main/java/kodlama/io/rentACar/core/utilities/exceptions/BusinessExeception.java
@@ -0,0 +1,9 @@
+package kodlama.io.rentACar.core.utilities.exceptions;
+
+public class BusinessExeception extends RuntimeException {
+
+	public BusinessExeception(String message) {
+		super(message);
+	}
+	
+}
